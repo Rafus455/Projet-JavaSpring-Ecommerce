dae8c161bb289d91d56b8e7f85ca6b2af582baaf
Arthur Brunault <143087813+Jatalux@users.noreply.github.com>
2026-01-20 10:19:28 +0100

    Feat : project initiation

---
+++ src/test/java/com/b2/e_commerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
