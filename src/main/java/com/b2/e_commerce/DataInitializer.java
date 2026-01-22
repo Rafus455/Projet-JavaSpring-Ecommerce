5dcd647b37878c5d821646f0562f336810c139e5
voltaroi <139485904+voltaroi@users.noreply.github.com>
2026-01-22 10:00:50 +0100

    Fix: permitAll

--- src/main/java/com/b2/e_commerce/DataInitializer.java
+++ src/main/java/com/b2/e_commerce/DataInitializer.java
@@ -1,16 +1,22 @@
 package com.b2.e_commerce;
 
 import com.b2.e_commerce.entity.Category;
+import com.b2.e_commerce.entity.Role;
+import com.b2.e_commerce.entity.User;
 import com.b2.e_commerce.repository.CategoryRepository;
+import com.b2.e_commerce.repository.RoleRepository;
+import com.b2.e_commerce.repository.UserRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner init(CategoryRepository categoryRepository) {
+    CommandLineRunner initCategories(CategoryRepository categoryRepository) {
         return args -> {
             if (categoryRepository.count() == 0) {
                 Category c = new Category();
@@ -19,6 +25,29 @@ public class DataInitializer {
                 c.setType("TECH");
                 categoryRepository.save(c);
             }
+        };
+    }
+
+    @Bean
+    CommandLineRunner initUsers(
+            RoleRepository roleRepo,
+            UserRepository userRepo,
+            PasswordEncoder encoder) {
+
+        return args -> {
+
+            // ROLE
+            Role userRole = new Role();
+            userRole.setRole("USER");
+            roleRepo.save(userRole);
+
+            // USER
+            User user = new User();
+            user.setMail("test@test.com");
+            user.setPassword(encoder.encode("1234"));
+            user.setRole(userRole);
+
+            userRepo.save(user);
         };
     }
 }
--- src/main/java/com/b2/e_commerce/config/SecurityConfig.java
+++ src/main/java/com/b2/e_commerce/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.b2.e_commerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +15,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
             )
             .csrf(csrf -> csrf
                 .ignoringRequestMatchers("/h2-console/**")
@@ -23,5 +25,10 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
