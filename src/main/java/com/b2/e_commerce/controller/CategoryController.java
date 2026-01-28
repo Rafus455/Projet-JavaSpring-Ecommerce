16b939978002c0667fa0eeec43dedd47e1b6bf5b
kwasnikrayan45 <kwasnikrayan45@gmail.com>
2026-01-28 09:53:57 +0100

    feat :  style css

--- src/main/java/com/b2/e_commerce/controller/CategoryController.java
+++ src/main/java/com/b2/e_commerce/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -27,17 +28,42 @@ public class CategoryController {
 
     @GetMapping("/{name}")
     public String category(@PathVariable String name, Model model) {
-
         Category category = categoryRepo.findByNameIgnoreCase(name);
         if (category == null) {
-            return "404";
+            return "404"; 
         }
 
         model.addAttribute("category", category);
-
         List<Product> products = productRepo.findByCategory(category);
         model.addAttribute("products", products);
+        model.addAttribute("allCategories", categoryRepo.findAll());
 
+        return "categorie";
+    }
+    
+    @GetMapping("/id/{id}")
+    public String voirCategorie(@PathVariable Long id,
+                                @RequestParam(required = false) Double min,
+                                @RequestParam(required = false) Double max,
+                                Model model) {
+      
+        Category category = categoryRepo.findById(id).orElse(null);
+        
+        if (category == null) {
+            return "redirect:/";
+        }
+        
+        double minPrice = (min != null) ? min : 0;
+        double maxPrice = (max != null) ? max : 1000000; 
+
+        List<Product> products = productRepo.findByCategoryAndPriceBetween(category, minPrice, maxPrice);
+        
+        model.addAttribute("category", category);
+        model.addAttribute("products", products);
+        
+        model.addAttribute("minPrice", minPrice);
+        model.addAttribute("maxPrice", maxPrice);
+        
         return "categorie";
     }
 }
